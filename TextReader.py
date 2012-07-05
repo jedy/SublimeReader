967112ef63a5239fc62ca890d7829e005181dda3
jedy <jedywu@gmail.com>
2012-07-05 22:27:45 +0800

    try to compatible to ConvertToUTF8

--- TextReader.py
+++ TextReader.py
@@ -1,6 +1,7 @@
 import sublime
 import sublime_plugin
 import hashlib
+import os.path
 
 CONFIG = "TextReader.sublime-settings"
 MIN_FILE_SIZE = 10240
@@ -12,28 +13,21 @@ class TextReader(sublime_plugin.EventListener):
         self.settings = sublime.load_settings(CONFIG)
 
     def on_load(self, view):
-        if view.settings().get('syntax', "").lower() != u'Packages/Text/Plain text.tmLanguage'.lower():
-            return
-        if view.size() < MIN_FILE_SIZE or view.file_name() in self.settings.get('not_use_reader', []):
-            return
-        view.set_syntax_file(u'Packages/TextReader/TextReader.tmLanguage')
-        view.set_read_only(True)
-        fig = self._digest(view)
-        if not fig:
-            return
-        pos = self.settings.get('history', {}).get(fig)
-        if not pos:
-            return
-        view.show(int(pos[1]))
-        view.show(int(pos[0]), False)
+        self._change_mode(view)
+
+    def on_modified(self, view):
+        if view.settings().get("waiting_detect_encode") and view.settings().get('origin_encoding'):
+            view.settings().erase("waiting_detect_encode")
+            view.erase_status("waiting_detect_encode")
+            self._change_mode(view)
 
     def on_close(self, view):
-        syntax = view.settings().get('syntax', "").lower()
-        if view.size() >= MIN_FILE_SIZE and syntax == u'Packages/Text/Plain text.tmLanguage'.lower():
+        syntax = view.settings().get('syntax', "")
+        if view.size() >= MIN_FILE_SIZE and syntax == u'Packages/Text/Plain text.tmLanguage':
             self._reset_not_use_reader(view, True)
             sublime.save_settings(CONFIG)
             return
-        if  syntax != u'Packages/TextReader/TextReader.tmLanguage'.lower():
+        if  syntax != u'Packages/TextReader/TextReader.tmLanguage':
             return
         self._reset_not_use_reader(view)
         fig = self._digest(view)
@@ -46,8 +40,31 @@ class TextReader(sublime_plugin.EventListener):
         self.settings.set('history', history)
         sublime.save_settings(CONFIG)
 
+    def _change_mode(self, view):
+        if view.settings().get('syntax', "") != u'Packages/Text/Plain text.tmLanguage':
+            return
+        if view.size() < MIN_FILE_SIZE or view.file_name() in self.settings.get('not_use_reader', []):
+            return
+        view_encoding = view.encoding()
+        if (view_encoding == 'Undefined' or view_encoding == sublime.load_settings('Preferences.sublime-settings').get('fallback_encoding')) \
+            and os.path.exists(os.path.join(sublime.packages_path(), "ConvertToUTF8")) \
+            and not view.settings().get('origin_encoding'):
+                view.set_status("waiting_detect_encode", "Wait ConvertToUTF8 to detect encode. You may choose TextReader syntax manually")
+                view.settings().set("waiting_detect_encode", True)
+                return
+        view.set_read_only(True)
+        view.set_syntax_file(u'Packages/TextReader/TextReader.tmLanguage')
+        fig = self._digest(view)
+        if not fig:
+            return
+        pos = self.settings.get('history', {}).get(fig)
+        if not pos:
+            return
+        view.show(int(pos[1]))
+        view.show(int(pos[0]), False)
+
     def _digest(self, view):
-        vsize = view.size()
+        vsize = min(10240, view.size())
         src = view.substr(sublime.Region(0, vsize)).strip()
         if not src:
             return
@@ -58,6 +75,8 @@ class TextReader(sublime_plugin.EventListener):
     def _reset_not_use_reader(self, view, add_cur_file=False):
         not_use_reader = self.settings.get('not_use_reader', [])
         fn = view.file_name()
+        if not fn:
+            return
         try:
             not_use_reader.remove(fn)
         except ValueError:
